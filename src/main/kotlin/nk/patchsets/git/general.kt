31d8deace9c853f59bf0ed39b86cc180969b629b
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-04-13 21:22:01 +0300

    Add reduce command to main interface

--- src/main/kotlin/nk/patchsets/git/general.kt
+++ src/main/kotlin/nk/patchsets/git/general.kt
@@ -6,17 +6,19 @@ package nk.patchsets.git.general
 import nk.patchsets.git.check.check
 import nk.patchsets.git.cleanup.cleanup
 import nk.patchsets.git.cp.cherryPick
+import nk.patchsets.git.reduce.reduce
 import nk.patchsets.git.restore.restore
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         System.err.println("""
-            Usage: cp|restore|cleanup|check <args>
+            Usage: cp|restore|cleanup|check|reduce <args>
 
             cp - for commits cherry pick
             restore - for restore branch state
             cleanup - removes all files with prefixes found in .bunch file
             check - check commits for forgotten bunch files updates
+            reduce - scan repository for unneeded files
 
             Example:
             <program> restore . 173_as31_as32
@@ -33,6 +35,7 @@ fun main(args: Array<String>) {
         "restore" -> restore(commandArgs)
         "cleanup" -> cleanup(commandArgs)
         "check" -> check(commandArgs)
+        "reduce" -> reduce(commandArgs)
         else -> {
             System.err.println("Unknown command: $command")
             return

85fba4cbc0575172061602e68b2783994a291259
xiexed <lkuka@yandex.ru>
2018-04-20 13:23:12 +0300

    *cherryPick* doc updates

--- src/main/kotlin/nk/patchsets/git/patch-cherry-pick.kt
+++ src/main/kotlin/nk/patchsets/git/patch-cherry-pick.kt
@@ -18,7 +18,7 @@ fun main(args: Array<String>) {
     cherryPick(args)
 }
 
-const val CHERRY_PICK_DESCRIPTION = "cherry-picks commit to master branch with auto-creating bunch files with given suffix."
+const val CHERRY_PICK_DESCRIPTION = "cherry-picks commit to the current branch with auto-creating bunch files with given suffix."
 
 fun cherryPick(args: Array<String>) {
     if (args.size != 4) {
@@ -29,11 +29,11 @@ fun cherryPick(args: Array<String>) {
 
             <git-path>  - Directory with repository (parent directory for .git folder).
             <since-ref> - Reference to most recent commit that should be ported.
-            <until-ref> - Parent of the last commit that should be ported.
+            <until-ref> - Parent of the last commit that should be ported (hash of "==== switch 182 ====" for instance).
             <suffix>    - Suffix for ported files.
 
             Example:
-            bunch cp C:/Projects/kotlin origin/master_182 c1125219e44aed60b8ba033ddae5b8f5c748052e 182
+            bunch cp C:/Projects/kotlin my-feature-branch c1125219e44aed60b8ba033ddae5b8f5c748052e 182
             """.trimIndent())
     }
 

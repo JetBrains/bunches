d4ec8f894198ae31bf0bab2d829fefc54b1c10e4
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-04-28 22:20:07 +0300

    Add options for committing files found in reduce command

--- src/main/kotlin/nk/patchsets/git/reduce.kt
+++ src/main/kotlin/nk/patchsets/git/reduce.kt
@@ -1,34 +1,73 @@
 package nk.patchsets.git.reduce
 
+import nk.patchsets.git.ChangeType
+import nk.patchsets.git.FileChange
+import nk.patchsets.git.commitChanges
 import nk.patchsets.git.file.readUpdatePairsFromFile
 import nk.patchsets.git.general.exitWithError
 import nk.patchsets.git.general.exitWithUsageError
 import nk.patchsets.git.restore.*
 import java.io.File
 
-data class Settings(val repoPath: String)
+enum class ReduceAction {
+    PRINT,
+    DELETE,
+    COMMIT
+}
+
+data class Settings(val repoPath: String, val action: ReduceAction, val commitMessage: String)
 
 fun main(args: Array<String>) {
     reduce(args)
 }
 
 const val REDUCE_DESCRIPTION = "Check repository for unneeded files with the same content."
+const val DEFAULT_REDUCE_COMMIT_TITLE = "~~~~ reduce ~~~~"
 
 fun reduce(args: Array<String>) {
-    if (args.size != 1) {
+    if (args.size !in 1..3) {
         exitWithUsageError("""
-            Usage: <git-path>
+            Usage: <git-path> [<action: print|delete|commit>] [<commit message>]
 
             $REDUCE_DESCRIPTION
 
-            <git-path> - Directory with repository (parent directory for .git)
+            <git-path>       - Directory with repository (parent directory for .git)
+            <action>         - Action that should be applied to redundant files. "commit" is used by default.
+                                 print - print the list in console
+                                 delete - only delete files
+                                 commit - delete files and commit them
+            <commit message> - Commit message for "commit" action. "~~~~ reduce ~~~~" is used by default.
 
             Example:
             bunch reduce C:/Projects/kotlin
             """.trimIndent())
     }
 
-    val settings = Settings(repoPath = args[0])
+    val actionFromParam = args.getOrNull(1)?.let {
+        when (it) {
+            "print" -> ReduceAction.PRINT
+            "delete" -> ReduceAction.DELETE
+            "commit" -> ReduceAction.COMMIT
+            else -> {
+                if (args.size == 3) {
+                    exitWithUsageError("Unknown action: '$it'")
+                }
+
+                null
+            }
+        }
+    }
+
+    if (args.size == 3 && actionFromParam != ReduceAction.COMMIT) {
+        exitWithUsageError("Commit message is ignored for '$actionFromParam'")
+    }
+
+    val commitMessageFromParam = args.getOrNull(if (actionFromParam == null) 1 else 2)
+
+    val settings = Settings(
+        repoPath = args[0],
+        action = actionFromParam ?: ReduceAction.COMMIT,
+        commitMessage = commitMessageFromParam ?: DEFAULT_REDUCE_COMMIT_TITLE)
 
     doReduce(settings)
 }
@@ -57,6 +96,7 @@ fun doReduce(settings: Settings) {
     val affectedOriginFiles: Set<File> =
             filesWithDonorExtensions.mapTo(HashSet(), { child -> File(child.parentFile, child.nameWithoutExtension) })
 
+    val reduceFiles = ArrayList<File>()
     for (affectedOriginFile in affectedOriginFiles) {
         val contentMap: Map<String, String?> = extensions.map { extension ->
             val file = if (extension == base) affectedOriginFile else affectedOriginFile.toBunchFile(extension)
@@ -75,12 +115,32 @@ fun doReduce(settings: Settings) {
                 val toContent = contentMap[toExtension] ?: continue
 
                 if (toContent == fromContent) {
-                    println(affectedOriginFile.toBunchFile(toExtension))
+                    reduceFiles.add(affectedOriginFile.toBunchFile(toExtension))
                 }
 
                 fromExtension = toExtension
             }
         }
+    }
+
+    if (settings.action == ReduceAction.PRINT) {
+        reduceFiles.sort()
+        for (file in reduceFiles) {
+            println(file)
+        }
+        return
+    }
+
+    assert(settings.action == ReduceAction.DELETE || settings.action == ReduceAction.COMMIT)
+
+    val changedFiles = ArrayList<FileChange>()
+    for (file in reduceFiles) {
+        file.delete()
+        changedFiles.add(FileChange(ChangeType.REMOVE, file))
+    }
+
+    if (settings.action == ReduceAction.COMMIT) {
+        commitChanges(settings.repoPath, changedFiles, settings.commitMessage)
     }
 }
 

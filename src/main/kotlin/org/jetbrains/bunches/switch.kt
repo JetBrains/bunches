d9eec7b6fbbcf907626fad498983e83ea18cd549
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-07-24 18:24:11 +0300

    Add an option for step-by-step switch

--- src/main/kotlin/org/jetbrains/bunches/switch.kt
+++ src/main/kotlin/org/jetbrains/bunches/switch.kt
@@ -14,12 +14,16 @@ import org.jetbrains.bunches.git.shouldIgnoreDir
 import java.io.File
 
 const val RESTORE_COMMIT_TITLE = "~~~~ switch {target} ~~~~"
+const val RESTORE_BACKUP_COMMIT_TITLE = "~~~~ backup files ~~~~"
 const val RESTORE_CLEANUP_COMMIT_TITLE = "~~~~ restore cleanup ~~~~"
+
+const val STEP_ = "--step"
 
 data class Settings(
         val repoPath: String,
         val rule: String,
         val commitTitle: String = RESTORE_COMMIT_TITLE,
+        val step: Boolean,
         val doCleanup: Boolean)
 
 fun main(args: Array<String>) {
@@ -33,32 +37,69 @@ const val SWITCH_DESCRIPTION = "Restores state of files for the particular branc
 fun restore(args: Array<String>) {
     if (args.size != 4 && args.size != 3 && args.size != 2) {
         exitWithUsageError("""
-            Usage: <git-path> <branches-rule> [<commit-title>] [$CLEAN_UP]
+            Usage: <git-path> <branches-rule> [$STEP_] [$CLEAN_UP] [<commit-title>]
 
             $SWITCH_DESCRIPTION
 
             <git-path>        - Directory with repository (parent directory for .git).
+
             <branches-rule>   - Set of file suffixes separated with `_` showing what files should be affected and priority
                                 of application. If only target branch is given file <git-path>/.bunch will be checked for
                                 pattern.
+
+            $STEP_            - Do switch step by step with intermediate commits after applying each step
+
+            $CLEAN_UP         - Remove bunch files after branch restore (executes 'cleanup' command with default arguments).
+
             <commit-title>    - Title for the switch commit. "$RESTORE_COMMIT_TITLE" is used by default. {target} pattern
                                 in message will be replaced with the target branch suffix.
-            $CLEAN_UP         - Remove bunch files after branch restore (executes 'cleanup' command with default arguments).
 
             Example:
             bunch switch C:/Projects/kotlin as32
             """.trimIndent())
     }
 
-    if (args.size == 4 && args[3] != CLEAN_UP) {
-        exitWithUsageError("Last parameter should be $CLEAN_UP or absent")
+    var commitTitle: String? = null
+    var doCleanup: Boolean? = null
+    var stepByStep: Boolean? = null
+
+    fun readOptionArg(value: String?) {
+        if (value == null) return
+        when (value) {
+            STEP_ -> {
+                if (stepByStep != null) {
+                    exitWithUsageError("Reassign of $STEP_ parameter")
+                }
+
+                stepByStep = true
+            }
+            CLEAN_UP -> {
+                if (doCleanup != null) {
+                    exitWithUsageError("Reassign of $CLEAN_UP parameter")
+                }
+
+                doCleanup = true
+            }
+            else -> {
+                if (commitTitle != null) {
+                    exitWithUsageError("Reassign of <commit-title> parameter")
+                }
+
+                commitTitle = value
+            }
+        }
     }
+
+    readOptionArg(args.getOrNull(2))
+    readOptionArg(args.getOrNull(3))
+    readOptionArg(args.getOrNull(4))
 
     val settings = Settings(
             repoPath = args[0],
             rule = args[1],
-            commitTitle = args.getOrNull(2)?.takeIf { it != CLEAN_UP } ?: RESTORE_COMMIT_TITLE,
-            doCleanup = CLEAN_UP == (args.getOrNull(3) ?: args.getOrNull(2))
+            commitTitle = commitTitle ?: RESTORE_COMMIT_TITLE,
+            step = stepByStep ?: false,
+            doCleanup = doCleanup ?: false
     )
 
     val suffixes = getRuleSuffixes(settings)
@@ -67,12 +108,113 @@ fun restore(args: Array<String>) {
     }
 
     if (suffixes.size != 1) {
-        doSwitch(suffixes, settings)
+        if (settings.step) {
+            doStepByStepSwitch(suffixes, settings)
+        } else {
+            doSwitch(suffixes, settings)
+        }
     }
 
     if (settings.doCleanup) {
         cleanup(org.jetbrains.bunches.cleanup.Settings(
                 settings.repoPath, extension = null, commitTitle =  RESTORE_CLEANUP_COMMIT_TITLE, isNoCommit = false))
+    }
+}
+
+private fun doStepByStepSwitch(suffixes: List<String>, settings: Settings) {
+    val originBranchExtension = suffixes.first()
+    val donorExtensionsInStepByStepOrder = suffixes.subList(1, suffixes.size).toSet()
+
+    val root = File(settings.repoPath)
+
+    if (!root.exists() || !root.isDirectory) {
+        exitWithError("Repository directory with branch is expected")
+    }
+
+    val filesWithDonorExtensions = root
+            .walkTopDown()
+            .onEnter { dir -> !shouldIgnoreDir(dir) }
+            .filter { child -> child.extension in donorExtensionsInStepByStepOrder }
+            .toList()
+
+    val affectedOriginFiles: Set<File> =
+            filesWithDonorExtensions.mapTo(HashSet()) { child -> File(child.parentFile, child.nameWithoutExtension) }
+
+    run {
+        val backupChanges = HashSet<FileChange>()
+        for (originFile in affectedOriginFiles) {
+            val baseCopiedFile = originFile.toBunchFile(originBranchExtension)
+            if (baseCopiedFile.exists()) {
+                exitWithError("Can't store copy of the origin file, because branch file is already exist: $baseCopiedFile")
+            }
+
+            if (originFile.exists()) {
+                if (originFile.isDirectory) {
+                    exitWithError("Bunching for directories is not supported: $originFile")
+                }
+
+                originFile.copyTo(baseCopiedFile)
+                backupChanges.add(
+                        FileChange(
+                                ChangeType.ADD,
+                                baseCopiedFile
+                        )
+                )
+            } else {
+                // File was absent and going to be introduced.
+                // Create empty bunch files to show it's going to be removed in old branch.
+                baseCopiedFile.createNewFile()
+                backupChanges.add(
+                        FileChange(
+                                ChangeType.ADD,
+                                baseCopiedFile
+                        )
+                )
+            }
+        }
+
+        commitChanges(
+                settings.repoPath,
+                backupChanges,
+                RESTORE_BACKUP_COMMIT_TITLE
+        )
+    }
+
+    for (extension in donorExtensionsInStepByStepOrder) {
+        val branchChanges = HashSet<FileChange>()
+        for (originFile in affectedOriginFiles) {
+            val targetFile = originFile.toBunchFile(extension)
+            if (!targetFile.exists()) {
+                continue
+            }
+
+            if (targetFile.isDirectory) {
+                exitWithError("Bunching for directories is not supported: $targetFile")
+            }
+
+            val isOriginExist = originFile.exists()
+
+            val isTargetRemoved = targetFile.readText().trim().isEmpty()
+            if (!isTargetRemoved) {
+                targetFile.copyTo(originFile, true)
+                branchChanges.add(FileChange(if (isOriginExist) ChangeType.MODIFY else ChangeType.ADD, originFile))
+            } else {
+                if (isOriginExist) {
+                    originFile.delete()
+                    branchChanges.add(FileChange(ChangeType.REMOVE, originFile))
+                } else {
+                    // That mean the bunch file is redundant, but do nothing
+                }
+            }
+        }
+
+        if (!branchChanges.isEmpty()) {
+            commitChanges(
+                    settings.repoPath,
+                    branchChanges,
+                    settings.commitTitle.replace("{target}", extension)
+            )
+        }
     }
 }
 

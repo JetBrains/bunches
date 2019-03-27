024c1336cfddb844a833d049964fa98ef283d655
darksidersstrife <vvllaadd99@yandex.ru>
2019-03-27 21:46:30 +0300

    Added git precommit hook template

---
+++ bunch-git-hook/src/main/kotlin/org/jetbrains/bunches/precommitHook/BunchPreCommitHook.kt
@@ -0,0 +1,58 @@
+package org.jetbrains.bunches.precommitHook
+
+import org.eclipse.jgit.api.Git
+import java.io.File
+import java.lang.System.exit
+import java.util.*
+
+fun getBunchExtensions(dotBunchFile: File): Set<String>? {
+    val lines = dotBunchFile.readLines().map { it.trim() }.filter { !it.isEmpty() }
+    if (lines.size <= 1) return null
+
+    return lines.drop(1).map { it.split('_').first() }.toSet()
+}
+
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        println("Precommit hook doesn't take any parameters")
+        exit(1)
+    }
+
+    val dotBunchFile = File(".bunch")
+    if (!dotBunchFile.exists()) {
+        println("Project's .bunch file wasn't found, hook is disabled")
+        exit(0)
+    }
+    val extensions = getBunchExtensions(dotBunchFile) ?: return
+
+    val git = Git.open(File(""))
+    val status = git.status().call()
+    val commitFiles = (status.added + status.changed + status.removed).map { File(it) }.toSet()
+
+    val forgottenFiles = HashSet<File>()
+
+    for (file in commitFiles) {
+        if (file.extension in extensions) continue
+
+        val parent = file.parent ?: continue
+        val name = file.name
+        for (extension in extensions) {
+            val bunchFile = File(parent, "$name.$extension")
+            if (bunchFile !in commitFiles && bunchFile.exists()) {
+                forgottenFiles.add(bunchFile)
+            }
+        }
+    }
+    if (forgottenFiles.isEmpty()) exit(0)
+
+    println("""
+        Some bunch files were not included in commit:
+        ${forgottenFiles.joinToString("\n")}
+        Do you want to continue? (Y/N)
+        """.trimIndent())
+    when (readLine()) {
+        "Y", "y", "Yes", "yes", "" -> exit(0)
+        "N", "n", "No", "no" -> exit(1)
+        else -> exit(1)
+    }
+}

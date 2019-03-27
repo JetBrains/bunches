729d2d7d65868b362633d8de8ddc9da55d7081a3
darksidersstrife <vvllaadd99@yandex.ru>
2019-03-27 21:46:30 +0300

    Added git precommit hook template.

---
+++ bunch-git-hook/src/main/java/org/jetbrains/bunches/precommitHook/BunchPreCommitHook.java
@@ -0,0 +1,13 @@
+package org.jetbrains.bunches.precommitHook;
+
+import static java.lang.System.exit;
+
+public class BunchPreCommitHook {
+    public static void main(String[] args){
+        if(args.length != 0){
+            exit(1);
+        }
+        System.out.println("Hello world");
+        exit(0);
+    }
+}

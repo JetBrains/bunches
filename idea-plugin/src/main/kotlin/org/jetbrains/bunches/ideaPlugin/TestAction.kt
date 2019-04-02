0a7f4cc36dfcc82598bf4af50fed5ae1a9afdf02
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-04-02 16:39:03 +0300

    Cleanup warnings in TestAction.kt

--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/TestAction.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/TestAction.kt
@@ -2,7 +2,6 @@ package org.jetbrains.bunches.ideaPlugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import org.jetbrains.bunches.ideaPlugin.*
 
 class TestAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -12,5 +11,4 @@ class TestAction : AnAction() {
         td.title = "Hello world!"
         td.isVisible = true
     }
-
 }

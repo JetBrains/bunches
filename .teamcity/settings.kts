456c313f68caba5a14a6ad7e45cb9af0f1a790bb
goodwinnk <goodwinnk@TeamCity>
2019-04-08 13:39:25 +0300

    Setup TeamCity build

---
+++ .teamcity/settings.kts
@@ -0,0 +1,69 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.ui.create
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_2.version
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2018.2"
+
+project {
+    description = "Bunch Tool"
+
+    vcsRoot(BunchTool)
+
+    buildType(Main)
+
+    cleanup {
+        preventDependencyCleanup = false
+    }
+}
+
+object Main : BuildType({
+    name = "Main"
+
+    vcs {
+        root(BunchTool)
+    }
+
+    steps {
+        gradle {
+            name = "Build All"
+            tasks = "clean build"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+
+    artifactRules = """
+        bunch-cli/build/distributions/*.zip
+    """.trimIndent()
+})
+
+object BunchTool : GitVcsRoot({
+    name = "Bunch Tool"
+    url = "https://github.com/JetBrains/bunches.git"
+})

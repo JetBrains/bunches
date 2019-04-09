bb9fee0ee3ae5e2a6539acf575d8bcbf96d434d7
Stoorx <antiproton2013@yandex.ru>
2019-04-09 19:19:29 +0300

    Add Bunch tool in Tools menu

---
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/BunchToolMenuGroup.kt
@@ -0,0 +1,5 @@
+package org.jetbrains.bunches.ideaPlugin
+
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+
+class BunchToolMenuGroup : DefaultActionGroup()
--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
@@ -3,12 +3,8 @@ package org.jetbrains.bunches.ideaPlugin
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class TestAction : AnAction() {
+class SwitchAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val td = TestDialog()
-        td.pack()
-        td.setLocationRelativeTo(null)
-        td.title = "Hello world!"
-        td.isVisible = true
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }

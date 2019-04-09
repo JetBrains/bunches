351c34db77a63691f2a03fe6186720927acbe71e
Stoorx <antiproton2013@yandex.ru>
2019-04-09 20:47:29 +0300

    Add Switch dialog

--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
@@ -2,9 +2,17 @@ package org.jetbrains.bunches.ideaPlugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.ui.components.JBList
+import javax.swing.DefaultListModel
+import javax.swing.JLabel
+import javax.swing.JList
 
 class SwitchAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val dialog = SwitchDialog()
+        dialog.pack()
+        dialog.setLocationRelativeTo(null)
+        dialog.title = "Switch bunches"
+        dialog.isVisible = true
     }
 }

c9c8b78507ea4c312b9d6c36c53bb5a36227bbb0
darksidersstrife <vvllaadd99@yandex.ru>
2019-04-12 22:46:07 +0300

    Minor fixes, changed gradle jvm back to 1.8

--- bunch-git-hook/src/main/kotlin/org/jetbrains/bunches/precommitHook/BunchPreCommitHook.kt
+++ bunch-git-hook/src/main/kotlin/org/jetbrains/bunches/precommitHook/BunchPreCommitHook.kt
@@ -1,7 +1,7 @@
 package org.jetbrains.bunches.precommitHook
 
-import org.eclipse.jgit.api.Git;
-import java.io.File;
+import org.eclipse.jgit.api.Git
+import java.io.File
 import java.lang.System.exit
 import java.util.*
 
@@ -13,26 +13,25 @@ fun getBunchExtensions(dotBunchFile: File): Set<String>? {
 }
 
 fun main(args: Array<String>) {
-    if (args.size !== 0) {
-        System.out.println("Precommit hook doesn't take any parameters")
+    if (args.isNotEmpty()) {
+        println("Precommit hook doesn't take any parameters")
         exit(1)
     }
 
     val dotBunchFile = File(".bunch")
     if (!dotBunchFile.exists()) {
-        System.out.println("Project's .bunch file wasn't found, hook is disabled")
-        //exit(0)
+        println("Project's .bunch file wasn't found, hook is disabled")
+        exit(0)
     }
-    val extensions = getBunchExtensions(dotBunchFile) ?: TreeSet<String>()
+    val extensions = getBunchExtensions(dotBunchFile) ?: return
 
     val git = Git.open(File(""))
     val status = git.status().call()
-    val commitFiles = (status.modified).map { File(it) }
+    val commitFiles = (status.added + status.changed + status.removed).map { File(it) }.toSet()
 
     val forgottenFiles = HashSet<File>()
 
     for (file in commitFiles) {
-        System.out.println(file)
         if (file.extension in extensions) continue
 
         val parent = file.parent ?: continue
@@ -46,9 +45,14 @@ fun main(args: Array<String>) {
     }
     if (forgottenFiles.isEmpty()) exit(0)
 
-    System.out.println("Some bunch files were not included in commit:\n${forgottenFiles.joinToString("\n")}\nDo you want to continue? (Yes/No)")
-    when (Scanner(System.`in`).nextLine()) {
-        "Yes" -> exit(0)
+    println("""
+        Some bunch files were not included in commit:
+        ${forgottenFiles.joinToString("\n")}
+        Do you want to continue? (Y/N)
+        """.trimIndent())
+    when (readLine()) {
+        "Y", "y", "Yes", "yes", "" -> exit(0)
+        "N", "n", "No", "no" -> exit(1)
         else -> exit(1)
     }
 }

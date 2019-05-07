c8c03d0853428197305b4580f7e7f1f5922ff5db
Stoorx <antiproton2013@yandex.ru>
2019-05-07 19:20:25 +0300

    Split SwitchDialog to _ and SwitchDialogKt

---
+++ idea-plugin/src/main/java/org/jetbrains/bunches/ideaPlugin/SwitchDialog.java
@@ -0,0 +1,26 @@
+package org.jetbrains.bunches.ideaPlugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+
+import javax.annotation.Nullable;
+import javax.swing.*;
+
+public class SwitchDialog extends DialogWrapper {
+    protected JPanel contentPane;
+    protected JComboBox<String> comboSwitch;
+    protected JCheckBox checkStepByStep;
+    protected JCheckBox checkCleanup;
+    protected JTextField textFieldCommitMessage;
+
+    public SwitchDialog(Project project) {
+        super(project, false);
+        init();
+    }
+
+    @Nullable
+    @Override
+    public JComponent createCenterPanel() {
+        return contentPane;
+    }
+}
--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchDialogKt.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchDialogKt.kt
@@ -1,21 +1,14 @@
 package org.jetbrains.bunches.ideaPlugin
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.DialogWrapper
 import org.jetbrains.bunches.restore.RESTORE_COMMIT_TITLE
-import javax.swing.*
+import javax.swing.JComponent
 
-class SwitchDialog(project: Project, bunches: List<String>?) : DialogWrapper(project, false) {
-    private lateinit var contentPane: JPanel
-    private lateinit var comboSwitch: JComboBox<String>
-    private lateinit var checkStepByStep: JCheckBox
-    private lateinit var checkCleanup: JCheckBox
-    private lateinit var textFieldCommitMessage: JTextField
-
+class SwitchDialogKt(project: Project, bunches: List<String>) : SwitchDialog(project) {
     init {
-        init()
         title = "Switch Bunches"
-        if (bunches != null && bunches.isNotEmpty()) bunches.forEach { comboSwitch.addItem(it) }
+        if (bunches.isNotEmpty())
+            bunches.forEach { comboSwitch.addItem(it) }
         else {
             comboSwitch.addItem("Nothing to show")
             comboSwitch.isEnabled = false
@@ -26,10 +19,6 @@ class SwitchDialog(project: Project, bunches: List<String>?) : DialogWrapper(pro
 
     override fun getPreferredFocusedComponent(): JComponent? {
         return comboSwitch
-    }
-
-    override fun createCenterPanel(): JComponent? {
-        return contentPane
     }
 
     fun getParameters(): SwitchParameters {

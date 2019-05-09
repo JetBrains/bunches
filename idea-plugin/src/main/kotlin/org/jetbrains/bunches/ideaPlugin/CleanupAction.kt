e01c8357ba0ff963badac88dd3aa5e350bbcbe5e
Nikolay Krasko <goodwinnk@gmail.com>
2019-05-09 17:33:02 +0300

    Use better api for background tasks

--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/CleanupAction.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/CleanupAction.kt
@@ -2,10 +2,12 @@ package org.jetbrains.bunches.ideaPlugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.ui.Messages
 import org.jetbrains.bunches.cleanup.Settings
+import org.jetbrains.bunches.cleanup.cleanup
 
 class CleanupAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -13,12 +15,12 @@ class CleanupAction : AnAction() {
 
         val basePath = project.basePath
         if (basePath == null) {
-            Messages.showMessageDialog("BasePath not found", "Switch error", Messages.getErrorIcon())
+            Messages.showMessageDialog("BasePath not found", "Cleanup error", Messages.getErrorIcon())
             return
         }
         val extensions = BunchFileUtils.bunchExtension(project)
         if (extensions == null) {
-            Messages.showMessageDialog("Can't find list of supported bunches", "Switch error", Messages.getErrorIcon())
+            Messages.showMessageDialog("Can't find list of supported bunches", "Cleanup error", Messages.getErrorIcon())
             return
         }
         val dialog = CleanupDialogKt(project, extensions)
@@ -34,11 +36,14 @@ class CleanupAction : AnAction() {
                 cleanupSettings.isNoCommit
         )
 
-        val task = BackgroundCleanup(project, settings)
-        val progressIndicator = BackgroundableProcessIndicator(task)
-        ProgressManager.getInstance().runProcessWithProgressAsynchronously(
-                task,
-                progressIndicator
-        )
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Bunch Cleanup", false) {
+            override fun run(indicator: ProgressIndicator) {
+                cleanup(settings)
+            }
+
+            override fun onThrowable(error: Throwable) {
+                Messages.showErrorDialog(project, error.message, "Cleanup error")
+            }
+        })
     }
 }
--- idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/bunches/ideaPlugin/SwitchAction.kt
@@ -2,13 +2,10 @@ package org.jetbrains.bunches.ideaPlugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.ui.Messages
-import org.jetbrains.bunches.BunchException
 import org.jetbrains.bunches.restore.Settings
 import org.jetbrains.bunches.restore.doSwitch
 
@@ -40,20 +37,16 @@ class SwitchAction : AnAction() {
                 switchSettings.stepByStep,
                 switchSettings.doCleanup)
 
-        val task = object : Task.Backgroundable(project, "Switch") {
-            override fun run(indicator: ProgressIndicator) {
-                indicator.isIndeterminate = true
-                try {
+        ProgressManager.getInstance().run(
+            object : Task.Backgroundable(project, "Bunch Switch", false) {
+                override fun run(indicator: ProgressIndicator) {
                     doSwitch(settings)
-                } catch (e: BunchException) {
-                    ApplicationManager.getApplication().invokeLater {
-                        Messages.showErrorDialog(project, e.message, "Switch error")
-                    }
+                }
+
+                override fun onThrowable(error: Throwable) {
+                    Messages.showErrorDialog(project, error.message, "Switch error")
                 }
             }
-        }
-
-        val progressIndicator = BackgroundableProcessIndicator(task)
-        ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, progressIndicator)
+        )
     }
 }

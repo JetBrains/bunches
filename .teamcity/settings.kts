a5d861d75f4ded2fb2a10156defba2a9a555c9a6
Nikolay Krasko <goodwinnk@gmail.com>
2019-06-18 01:36:19 +0300

    Versioning during plugin publishing

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,28 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.project
 import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2018.2"
 
 project {
@@ -42,6 +20,7 @@ project {
 
 object Main : BuildType({
     name = "Main"
+    buildNumberPattern = "1.0.%build.counter%"
 
     vcs {
         root(BunchTool)
@@ -50,7 +29,7 @@ object Main : BuildType({
     steps {
         gradle {
             name = "Build All"
-            tasks = "clean build"
+            tasks = "clean build -P buildNumber %build.number%"
             buildFile = ""
             gradleWrapperPath = ""
         }

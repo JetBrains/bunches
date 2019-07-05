9e6c51c191b5c7a4535ece24a369be12747f50a6
DmiitriiJarosh <dimajarosh@gmail.com>
2019-07-05 19:11:46 +0300

    Cleanup and general use Clikt

--- bunch-cli/src/main/kotlin/org/jetbrains/bunches/general.kt
+++ bunch-cli/src/main/kotlin/org/jetbrains/bunches/general.kt
@@ -3,12 +3,22 @@
 
 package org.jetbrains.bunches.general
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.findObject
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.ajalt.clikt.parameters.types.path
 import org.jetbrains.bunches.BunchException
 import org.jetbrains.bunches.BunchParametersException
 import org.jetbrains.bunches.ManifestReader
 import org.jetbrains.bunches.check.CHECK_DESCRIPTION
 import org.jetbrains.bunches.check.check
 import org.jetbrains.bunches.cleanup.CLEANUP_DESCRIPTION
+import org.jetbrains.bunches.cleanup.CleanUp
 import org.jetbrains.bunches.cleanup.cleanup
 import org.jetbrains.bunches.cp.CHERRY_PICK_DESCRIPTION
 import org.jetbrains.bunches.cp.cherryPick
@@ -19,6 +29,7 @@ import org.jetbrains.bunches.restore.SWITCH_DESCRIPTION
 import org.jetbrains.bunches.restore.restore
 import org.jetbrains.bunches.stats.STATS_DESCRIPTION
 import org.jetbrains.bunches.stats.stats
+import java.nio.file.Path
 import kotlin.system.exitProcess
 
 fun exitWithUsageError(message: String): Nothing {
@@ -29,20 +40,20 @@ fun exitWithError(message: String? = null): Nothing {
     throw BunchException(message)
 }
 
-inline fun process(args: Array<String>, f: (Array<String>) -> Unit) {
+inline fun process(verbose: Boolean, f: () -> Unit) {
     try {
-        f(args)
+        f()
     } catch (e: BunchParametersException) {
-        printExceptionToSystemError(1, args, e)
+        printExceptionToSystemError(1, verbose, e)
     } catch (e: BunchException) {
-        printExceptionToSystemError(2, args, e)
+        printExceptionToSystemError(2, verbose, e)
     } catch (e: Throwable) {
-        printExceptionToSystemError(3, args, e)
+        printExceptionToSystemError(3, verbose, e)
     }
 }
 
-fun printExceptionToSystemError(errorCode: Int, args: Array<String>, e: Throwable): Nothing {
-    if (args.contains("--verbose")) {
+fun printExceptionToSystemError(errorCode: Int, verbose: Boolean, e: Throwable): Nothing {
+    if (verbose) {
         System.err.println(e)
         e.printStackTrace()
     } else {
@@ -51,50 +62,67 @@ fun printExceptionToSystemError(errorCode: Int, args: Array<String>, e: Throwabl
     exitProcess(errorCode)
 }
 
+val commands = listOf(CleanUp())
+
+fun <A, B> Function1<A, B>.partial(a: A): () -> B {
+    return {invoke(a)}
+}
+
 fun main(args: Array<String>) {
-    process(args, ::doMain)
+    val a2rgs = readLine()?.split(" ")?.toTypedArray()!!
+    BunchCli().subcommands(commands).main(a2rgs)
 }
 
-fun doMain(args: Array<String>) {
-    if (args.isEmpty()) {
-        exitWithUsageError(
-            """
-            Usage: switch|cp|cleanup|check|reduce|stats|--version <args>
-
-            switch  - $SWITCH_DESCRIPTION
-            cp      - $CHERRY_PICK_DESCRIPTION
-            cleanup - $CLEANUP_DESCRIPTION
-            check   - $CHECK_DESCRIPTION
-            reduce  - $REDUCE_DESCRIPTION
-            stats   - $STATS_DESCRIPTION
-
-            Example:
-            bunch switch . as32
-            """.trimIndent()
-        )
+class BunchCli : CliktCommand(name = "bunch") {
+    init {
+        versionOption(getVersion())
     }
-
-    val command = args[0]
-    val commandArgs = args.toList().drop(1).toTypedArray()
-    when (command) {
-        "cp" -> cherryPick(commandArgs)
-        "restore" -> restore(commandArgs)
-        "switch" -> restore(commandArgs)
-        "cleanup" -> cleanup(commandArgs)
-        "check" -> check(commandArgs)
-        "reduce" -> reduce(commandArgs)
-        "stats" -> stats(commandArgs)
-        "installHook" -> installHookCommand(commandArgs)
-        "uninstallHook" -> uninstallHook(commandArgs)
-        BUNCH_PRE_COMMIT_CHECK_COMMAND -> precommitHook(commandArgs)
-        BUNCH_PRE_PUSH_CHECK_COMMAND -> checkBeforePush(commandArgs)
-        BUNCH_PRE_REBASE_CHECK_COMMAND -> checkPreRebase(commandArgs)
-        "--version" -> printVersion()
-
-        else -> throw BunchParametersException("Unknown command: $command")
+    val verbose by option("--verbose").flag()
+    val config by findObject { mutableMapOf<String, Boolean>() }
+    override fun run() {
+        config["VERBOSE"] = verbose
     }
 }
 
-private fun printVersion() {
-    println(ManifestReader.readAttribute("Bundle-Version") ?: "unknown")
-}
+//fun doMain(args: Array<String>) {
+//    if (args.isEmpty()) {
+//        exitWithUsageError(
+//            """
+//            Usage: switch|cp|cleanup|check|reduce|stats|--version <args>
+//
+//            switch  - $SWITCH_DESCRIPTION
+//            cp      - $CHERRY_PICK_DESCRIPTION
+//            cleanup - $CLEANUP_DESCRIPTION
+//            check   - $CHECK_DESCRIPTION
+//            reduce  - $REDUCE_DESCRIPTION
+//            stats   - $STATS_DESCRIPTION
+//
+//            Example:
+//            bunch switch . as32
+//            """.trimIndent()
+//        )
+//    }
+//
+//    val command = args[0]
+//    val commandArgs = args.toList().drop(1).toTypedArray()
+//    when (command) {
+//        "cp" -> cherryPick(commandArgs)
+//        "restore" -> restore(commandArgs)
+//        "switch" -> restore(commandArgs)
+//        "cleanup" -> cleanup(commandArgs)
+//        "check" -> check(commandArgs)
+//        "reduce" -> reduce(commandArgs)
+//        "stats" -> stats(commandArgs)
+//        "installHook" -> installHookCommand(commandArgs)
+//        "uninstallHook" -> uninstallHook(commandArgs)
+//        BUNCH_PRE_COMMIT_CHECK_COMMAND -> precommitHook(commandArgs)
+//        BUNCH_PRE_PUSH_CHECK_COMMAND -> checkBeforePush(commandArgs)
+//        BUNCH_PRE_REBASE_CHECK_COMMAND -> checkPreRebase(commandArgs)
+//        "--version" -> printVersion()
+//
+//        else -> throw BunchParametersException("Unknown command: $command")
+//    }
+//}
+
+private fun getVersion() = ManifestReader.readAttribute("Bundle-Version") ?: "unknown"
+

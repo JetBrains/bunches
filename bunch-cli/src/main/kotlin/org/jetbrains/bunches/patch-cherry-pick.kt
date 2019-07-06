befcd61c9c02b7a112a1837de7e17207fa1de9de
DmiitriiJarosh <dimajarosh@gmail.com>
2019-07-06 20:57:05 +0300

    Cp and Switch CLI

--- bunch-cli/src/main/kotlin/org/jetbrains/bunches/patch-cherry-pick.kt
+++ bunch-cli/src/main/kotlin/org/jetbrains/bunches/patch-cherry-pick.kt
@@ -3,9 +3,17 @@
 
 package org.jetbrains.bunches.cp
 
-import org.jetbrains.bunches.general.exitWithUsageError
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.requireObject
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
+import org.jetbrains.bunches.general.partial
+import org.jetbrains.bunches.general.process
 import org.jetbrains.bunches.git.reCommitPatched
 import org.jetbrains.bunches.git.readCommits
+import java.nio.file.Paths
 
 data class Settings(
     val gitPath: String,
@@ -15,36 +23,37 @@ data class Settings(
 )
 
 fun main(args: Array<String>) {
-    cherryPick(args)
+    CherryPickCommand().main(args)
 }
 
 const val CHERRY_PICK_DESCRIPTION =
     "cherry-picks commit to the current branch with auto-creating bunch files with given suffix."
 
+val CHERRY_PICK_EXAMPLE =
+    """
+    Example:
+    bunch cp -C C:/Projects/kotlin my-feature-branch c1125219e44aed60b8ba033ddae5b8f5c748052e 182
+    """.trimIndent()
+
 const val CP_SINCE = "since-ref"
 const val CP_UNTIL = "until-ref"
-const val CP_SX = "suffix"
+const val CP_SX = "extension"
 
-fun cherryPick(args: Array<String>) {
-    if (args.size != 4) {
-        exitWithUsageError(
-            """
-            Usage: <git-path> <since-ref> <until-ref> <suffix>
-
-            $CHERRY_PICK_DESCRIPTION
-
-            <git-path>  - Directory with repository (parent directory for .git folder).
-            <$CP_SINCE> - Reference to most recent commit that should be ported.
-            <$CP_UNTIL> - Parent of the last commit that should be ported (hash of "==== switch 182 ====" for instance).
-            <$CP_SX>    - Suffix for ported files.
-
-            Example:
-            bunch cp C:/Projects/kotlin my-feature-branch c1125219e44aed60b8ba033ddae5b8f5c748052e 182
-            """.trimIndent()
-        )
+class CherryPickCommand : CliktCommand(name = "cp", help = CHERRY_PICK_DESCRIPTION, epilog = CHERRY_PICK_EXAMPLE) {
+    val config by requireObject<Map<String, Boolean>>()
+    val repoPath by option("-C", help = "Directory with repository (parent directory for .git).")
+        .path(exists = true, fileOkay = false)
+        .default(Paths.get(".").toAbsolutePath().normalize())
+    val sinceRef by argument(name = "<$CP_SINCE>",  help = "Reference to most recent commit that should be ported.")
+    val untilRef by argument(name = "<$CP_UNTIL>", help = "Parent of the last commit that should be ported (hash of \"==== switch 182 ====\" for instance).")
+    val extension by argument(name = "<$CP_SX>", help = "Suffix for ported files.")
+    override fun run() {
+        val settings = Settings(repoPath.toString(), sinceRef, untilRef, extension)
+        process(config.getValue("VERBOSE"), ::cherryPick.partial(settings))
     }
+}
 
-    val settings = Settings(args[0], args[1], args[2], args[3])
+fun cherryPick(settings: Settings) {
     with(settings) {
         val commits = readCommits(gitPath, branch, untilHash)
 

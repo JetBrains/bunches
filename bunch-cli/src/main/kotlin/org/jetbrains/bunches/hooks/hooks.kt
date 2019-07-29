869bde481594dfc5bda31cf75cfea6bfcc441466
Darya Razumova <d.razumova@yandex.ru>
2019-07-29 14:51:50 +0300

    Added hooks update action and all type

--- bunch-cli/src/main/kotlin/org/jetbrains/bunches/hooks/hooks.kt
+++ bunch-cli/src/main/kotlin/org/jetbrains/bunches/hooks/hooks.kt
@@ -3,21 +3,51 @@
 package org.jetbrains.bunches.hooks
 
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.types.choice
+import org.jetbrains.bunches.BunchException
 import org.jetbrains.bunches.general.BunchSubCommand
 import org.jetbrains.bunches.general.exitWithError
 import java.io.File
 
 enum class HookAction {
-    INSTALL,
-    UNINSTALL
+    INSTALL {
+        override val action = "install"
+        override val help = "install hooks and save old hook if necessary." +
+                " It such hook already exits it will be replaced with current version.\n"
+    },
+    UNINSTALL {
+        override val action = "uninstall"
+        override val help = "uninstall hook and restore old hook if it exists."
+    },
+    UPDATE {
+        override val action = "update"
+        override val help = "update hook if it exists."
+    };
+
+    abstract val action: String
+    abstract val help: String
 }
 
 class HooksSettings(
     val path: String,
-    val type: HookType,
+    val type: HookTypeOption,
     val action: HookAction
 )
+
+class HookTypeOption(private val hookType: HookType?) {
+    companion object {
+        val allType = HookTypeOption(null)
+    }
+
+    fun getTypeList(): List<HookType> {
+        return if (hookType != null) {
+            listOf(hookType)
+        } else {
+            HookType.values().toList()
+        }
+    }
+}
 
 class HooksCommand : BunchSubCommand(
     name = "hooks",
@@ -25,13 +55,18 @@ class HooksCommand : BunchSubCommand(
 ) {
     val repoPath by repoPathOption()
 
-    private val action: HookAction by argument("action", help = "Action: install or uninstall.")
-        .choice("install" to HookAction.INSTALL, "uninstall" to HookAction.UNINSTALL)
-
-    private val hookType: HookType by argument(
-        "hook-type", help = "Type: ${HookType.values().joinToString { it.hookName }}."
+    private val action: HookAction by argument(
+        "action",
+        help = "Action:\n${HookAction.values().joinToString("\n") { "${it.action} -  ${it.help}" }}."
     )
-        .choice(HookType.values().map { it.hookName to it }.toMap())
+        .choice(HookAction.values().map { it.action to it }.toMap())
+
+    private val hookType: HookTypeOption by argument(
+        "hook-type", help = "Type: ${HookType.values().joinToString { it.hookName }}. All used by default."
+    ).choice(HookType.values()
+        .associate { it.hookName to HookTypeOption(it) }.plus("all" to HookTypeOption.allType)
+    )
+        .default(HookTypeOption.allType)
 
     override fun run() {
         val settings = HooksSettings(
@@ -58,10 +93,34 @@ fun hookCommand(settings: HooksSettings) {
 }
 
 fun doHookCommand(settings: HooksSettings) {
-    when (settings.action) {
-        HookAction.INSTALL -> installHook(settings.type, File(settings.path))
-        HookAction.UNINSTALL -> uninstallHook(settings.type, File(settings.path))
+    val dotGitPath = File(settings.path, ".git")
+    for (type in settings.type.getTypeList()) {
+        try {
+            when (settings.action) {
+                HookAction.INSTALL -> installHook(type, dotGitPath)
+                HookAction.UNINSTALL -> uninstallHook(type, dotGitPath)
+                HookAction.UPDATE -> updateHook(type, dotGitPath)
+            }
+        } catch (exception: LocalBunchException) {
+            println(exception.message)
+        }
     }
+}
+
+fun updateHook(type: HookType, dotGitPath: File) {
+    val hooksDirectory = File(dotGitPath, "hooks")
+    val hookPath = File(hooksDirectory, type.hookName)
+    if (!hookPath.exists() || !checkHookMarker(hookPath.readText(), type)) {
+        exitWithLocalError("No ${type.hookName} hook found")
+    }
+
+    val bunchExecutableFile = findExecutableFileFromZip()
+        ?: findExecutableFileFromProject()
+        ?: exitWithError("Can't find executable file")
+
+    val oldHookPath = hookCodeParams(hookPath.readText()).oldHookPath
+    hookPath.writeText(type.getHookCodeTemplate(bunchExecutableFile, oldHookPath, dotGitPath.parentFile))
+    println("Successfully updated ${type.hookName} hook")
 }
 
 fun installHook(type: HookType, dotGitPath: File) {
@@ -73,7 +132,7 @@ fun installHook(type: HookType, dotGitPath: File) {
 
     if (File(hookPath).exists()) {
         if (checkHookMarker(File(hookPath).readText(), type)) {
-            println("Bunch file checking hook found, it will be reinstalled")
+            println("Bunch file ${type.hookName} hook found, it will be reinstalled")
             val oldHookName = File(hookCodeParams(File(hookPath).readText()).oldHookPath).name
             oldHookNewName = if (oldHookName == ":") "" else oldHookName
         } else {
@@ -88,7 +147,7 @@ fun installHook(type: HookType, dotGitPath: File) {
 
             when (readLine()) {
                 "2" -> {
-                    oldHookNewName = readLine() ?: exitWithError("New name was not provided")
+                    oldHookNewName = readLine() ?: exitWithLocalError("New name for ${type.hookName} was not provided")
                 }
                 else -> {
                     val tempFile = createTempFile(type.hookName, "", File("$dotGitPath/hooks"))
@@ -100,7 +159,7 @@ fun installHook(type: HookType, dotGitPath: File) {
             if (File(hookPath).renameTo(File(hooksPath, oldHookNewName)))
                 println("Old hook was renamed to $oldHookNewName and will still be called")
             else
-                exitWithError("Couldn't rename existing hook")
+                exitWithLocalError("Couldn't rename existing ${type.hookName} hook")
         }
     } else {
         oldHookNewName = ""
@@ -108,10 +167,10 @@ fun installHook(type: HookType, dotGitPath: File) {
 
     val hookFile = File(hookPath).absoluteFile
     if (!hookFile.exists() && !hookFile.createNewFile()) {
-        exitWithError("Failed to create hook file")
+        exitWithLocalError("Failed to create ${type.hookName} hook file")
     }
     if (!hookFile.setExecutable(true)) {
-        exitWithError("Failed to make hook executable")
+        exitWithLocalError("Failed to make ${type.hookName} hook executable")
     }
 
     val bunchExecutableFile = findExecutableFileFromZip()
@@ -132,28 +191,28 @@ fun uninstallHook(type: HookType, dotGitPath: File) {
     val hooksPath = File(dotGitPath, "hooks")
     val hookFile = File(hooksPath, type.hookName)
     if (!hookFile.exists()) {
-        exitWithError("${type.hookName} hook is not found")
+        exitWithLocalError("${type.hookName} hook is not found")
     }
 
     val hookCode = hookFile.readText()
 
     if (!checkHookMarker(hookCode, type)) {
-        exitWithError("${type.hookName} hook exists but this is not the bunch tool hook")
+        exitWithLocalError("${type.hookName} hook exists but this is not the bunch tool hook")
     }
 
     val hookParams = hookCodeParams(hookCode)
     if (!hookFile.delete()) {
-        exitWithError("Couldn't delete hook file")
+        exitWithLocalError("Couldn't delete ${type.hookName} hook file")
     }
 
     if (hookParams.oldHookPath == ":")
-        println("Successfully uninstalled hook")
+        println("Successfully uninstalled ${type.hookName} hook")
     else {
         val oldHookFile = File(hookParams.oldHookPath)
         if (oldHookFile.renameTo(File(hooksPath, type.hookName)))
             println("Successfully uninstalled. Old ${type.hookName} hook was reverted.")
         else
-            exitWithError("Successfully uninstalled. Old ${type.hookName} hook wasn't restored.")
+            exitWithLocalError("Successfully uninstalled. Old ${type.hookName} hook wasn't restored.")
     }
 }
 
@@ -174,3 +233,9 @@ fun findExecutableFileFromProject(): File? {
     }
     return null
 }
+
+private fun exitWithLocalError(message: String? = null): Nothing {
+    throw LocalBunchException(message)
+}
+
+private class LocalBunchException(message: String? = null) : BunchException(message)

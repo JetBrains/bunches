bca6b5f7053bed478e7cdf7a1d5419693a75bf0e
DmiitriiJarosh <dimajarosh@gmail.com>
2019-08-11 16:49:07 +0300

    Refactoring

--- bunch-cli/src/main/kotlin/org/jetbrains/bunches/log.kt
+++ bunch-cli/src/main/kotlin/org/jetbrains/bunches/log.kt
@@ -13,13 +13,10 @@ import org.eclipse.jgit.revwalk.filter.CommitTimeRevFilter
 import org.jetbrains.bunches.file.readExtensionsFromFile
 import org.jetbrains.bunches.file.resultWithExit
 import org.jetbrains.bunches.general.BunchSubCommand
-import org.jetbrains.bunches.git.CommitInfo
-import org.jetbrains.bunches.git.FileAction
+import org.jetbrains.bunches.general.exitWithError
 import org.jetbrains.bunches.git.readCommits
 import java.util.*
 import java.util.Calendar
-
-
 
 enum class LogType {
     DAY,
@@ -80,89 +77,81 @@ class LogCommand : BunchSubCommand(
     }
 }
 
-fun getStartDate(calendarPeriod: Int, amount: Int): Date {
+private fun getStartDate(calendarPeriod: Int, amount: Int): Date {
     val calendar = Calendar.getInstance()
     calendar.time = Date()
     calendar.add(calendarPeriod, -amount)
     return calendar.time
 }
 
-fun LogCommand.addRevFilter(since: Date): LogCommand {
-    val revFilter = CommitTimeRevFilter.after(since)
-    setRevFilter(revFilter)
-    return this
-}
+private fun typeLogToCalendar(type: LogType): Int =
+    when(type) {
+        LogType.DAY -> Calendar.DAY_OF_YEAR
+        LogType.WEEK -> Calendar.WEEK_OF_YEAR
+        LogType.MONTH -> Calendar.MONTH
+        LogType.YEAR -> Calendar.YEAR
+        else -> exitWithError("Can't convert LASTCOMMITS log type to Calendar type.")
+    }
 
-fun isBunchAdditionDeletionInCommit(commit: CommitInfo, extensions: List<String>): Boolean =
-        commit.fileActions.any { action: FileAction -> (
-                action.changeType == DiffEntry.ChangeType.ADD
-                || action.changeType == DiffEntry.ChangeType.DELETE
-                )
-                && action.newPath?.endsWithAnyOf(extensions) ?: false
+private fun getGitLogFilter(settings: Settings): LogCommand.(Git) -> LogCommand =
+    when (settings.kind) {
+        LogType.LASTCOMMITS -> {
+            { this.setMaxCount(settings.amount) }
         }
+        else -> {
+            val typeOfPeriod = typeLogToCalendar(settings.kind)
+            val startDate = getStartDate(typeOfPeriod, settings.amount)
+            val revFilter = CommitTimeRevFilter.after(startDate);
+            { this.setRevFilter(revFilter)}
+        }
+    }
 
-fun String.endsWithAnyOf(endings: List<String>): Boolean =
-    endings.map { this.endsWith(it) }.any { it }
-
-fun doLogStats(settings: Settings) {
+private fun doLogStats(settings: Settings) {
     val extensions = readExtensionsFromFile(settings.repoPath)
         .resultWithExit()
         .toSet()
         .map { ".$it" }
 
-    val gitLogFilter =
-        when (settings.kind) {
-            LogType.LASTCOMMITS -> { it: LogCommand, _: Git -> it.setMaxCount(settings.amount) }
-            LogType.DAY -> {
-                val startDate = getStartDate(Calendar.DAY_OF_YEAR, settings.amount);
-                { it: LogCommand, _: Git -> it.addRevFilter(startDate)}
-            }
-            LogType.WEEK -> {
-                val startDate = getStartDate(Calendar.WEEK_OF_YEAR, settings.amount);
-                { it: LogCommand, _: Git -> it.addRevFilter(startDate)}
-            }
-            LogType.MONTH -> {
-                val startDate = getStartDate(Calendar.MONTH, settings.amount);
-                { it: LogCommand, _: Git -> it.addRevFilter(startDate)}
-            }
-            LogType.YEAR -> {
-                val startDate = getStartDate(Calendar.YEAR, settings.amount);
-                { it: LogCommand, _: Git -> it.addRevFilter(startDate)}
-            }
-        }
+    fun isBunchFile(filename: String?): Boolean =
+        extensions.any { filename?.endsWith(it) ?: false }
 
-    var commits = readCommits(settings.repoPath, gitLogFilter)
-    val amountOfProcessedCommits = commits.size
-    commits = commits.filter {
-        isBunchAdditionDeletionInCommit(it, extensions)
-    }
+    val gitLogFilter = getGitLogFilter(settings)
 
-    println("%d commits processed".format(amountOfProcessedCommits))
+    val commits = readCommits(settings.repoPath, gitLogFilter)
+
+    println("%d commits processed".format(commits.size))
     for (commit in commits) {
-        val amountOfAddedFiles = commit.fileActions.count {
+        val addedCount = commit.fileActions.count {
             it.changeType == DiffEntry.ChangeType.ADD
-                    && it.newPath?.endsWithAnyOf(extensions) ?: false
+                    && isBunchFile(it.newPath)
         }
-        val amountOfDeletedFiles = commit.fileActions.count {
+        val deletedCount = commit.fileActions.count {
             it.changeType == DiffEntry.ChangeType.DELETE
-                    && it.newPath?.endsWithAnyOf(extensions) ?: false
+                    && isBunchFile(it.newPath)
         }
+
+        if (addedCount == 0 && deletedCount == 0) {
+            continue
+        }
+
         val authorName = commit.author?.name ?: "unknown author"
         val commitTime = commit.author?.`when` ?: "unknown time"
 
-        if (amountOfAddedFiles != 0) {
-            print("+%d ".format(amountOfAddedFiles))
+        if (addedCount != 0) {
+            print("+%d ".format(addedCount))
         }
 
-        if (amountOfDeletedFiles != 0) {
-            print("-%d ".format(amountOfDeletedFiles))
+        if (deletedCount != 0) {
+            print("-%d ".format(deletedCount))
         }
 
-        print("%.11s %s %s %s".format(
-            commit.hash,
-            authorName,
-            commitTime,
-            commit.message
-        ))
+        println(
+            "%.11s %s %s %s".format(
+                commit.hash,
+                authorName,
+                commitTime,
+                commit.title
+            )
+        )
     }
 }

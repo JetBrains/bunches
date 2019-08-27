00a7c068b6c2e7e5825d6970222a71a7d631318f
Darya Razumova <d.razumova@yandex.ru>
2019-08-27 17:35:13 +0300

    Compare with local main file action update

--- idea-plugin/src/main/java/org/jetbrains/bunches/idea/vcs/DiffWithMainForDeletedActionProvider.kt
+++ idea-plugin/src/main/java/org/jetbrains/bunches/idea/vcs/DiffWithMainForDeletedActionProvider.kt
@@ -3,59 +3,64 @@ package org.jetbrains.bunches.idea.vcs
 import com.intellij.diff.DiffContentFactoryImpl
 import com.intellij.diff.DiffManager
 import com.intellij.diff.requests.DiffRequest
+import com.intellij.diff.requests.ErrorDiffRequest
 import com.intellij.diff.requests.SimpleDiffRequest
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.AnActionExtensionProvider
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.ContentRevision
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.bunches.idea.util.BunchFileUtils
 import java.io.File
 
 @Suppress("UnstableApiUsage")
-class DiffWithMainForDeletedActionProvider : AnAction(), DumbAware, AnActionExtensionProvider {
+class DiffWithMainForDeletedActionProvider : AnAction(
+    "Compare with main file",
+    "Compare bunch file with the local main file",
+    AllIcons.Vcs.Branch
+) {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return
 
         val change = e.getChange() ?: return
-        val revision = change.afterRevision ?: change.beforeRevision ?: return
+        val revision = change.getRevision() ?: return
         val file = revision.file
-        val mainFile = getMainFile(e.project, file.ioFile.absolutePath) ?: return
+        val mainFile = getMainFile(e.project, file.ioFile.absolutePath)
 
         val text = revision.content ?: return
         val diffContentFactory = DiffContentFactoryImpl.getInstance()
-        val request: DiffRequest = SimpleDiffRequest(
-            "Comparison with local main file ${mainFile.path}", diffContentFactory.create(project, mainFile),
-            diffContentFactory.create(text),
-            "Local ${mainFile.name}", "${file.ioFile.name} from ${revision.revisionNumber.asString()}"
-        )
 
+        val request: DiffRequest =
+            if (mainFile != null) {
+                SimpleDiffRequest(
+                    "Comparison with the local main file ${mainFile.path}", diffContentFactory.create(project, mainFile),
+                    diffContentFactory.create(text),
+                    "Local ${mainFile.name}", "${file.name} from ${revision.revisionNumber.asString()}"
+                )
+            } else {
+                ErrorDiffRequest(
+                    "Comparison with the local main file ${file.parentPath}${file.ioFile.nameWithoutExtension}",
+                    "Failed to find local main file content"
+                )
+            }
         DiffManager.getInstance().showDiff(project, request)
     }
 
     override fun update(e: AnActionEvent) {
-        if (isActive(e)) {
-            e.presentation.icon = AllIcons.Vcs.Branch
-            e.presentation.text = "Compare with main file"
-            e.presentation.description = "Compare bunch file with local main file"
-        }
+        e.presentation.isEnabled = isActive(e)
     }
 
-    override fun isActive(e: AnActionEvent): Boolean {
-        val change = e.getChange() ?: return false
-
-        val deletedFile = change.afterRevision?.file ?: change.beforeRevision?.file ?: return false
-        if (deletedFile.ioFile.exists()) return false
-
-        getMainFile(e.project, deletedFile.ioFile.absolutePath) ?: return false
-        return true
+    private fun isActive(e: AnActionEvent): Boolean {
+        val revision = e.getChange()?.getRevision() ?: return false
+        val extension = revision.file.ioFile.extension
+        val bunchExtensions = BunchFileUtils.bunchExtensions(e.project ?: return false) ?: return false
+        return extension in bunchExtensions
     }
 
     private fun AnActionEvent.getChange(): Change? {
@@ -71,5 +76,9 @@ class DiffWithMainForDeletedActionProvider : AnAction(), DumbAware, AnActionExte
         }
         val mainFile = File(bunchFile.parentFile.absolutePath, bunchFile.nameWithoutExtension)
         return LocalFileSystem.getInstance().findFileByIoFile(mainFile)
+    }
+
+    private fun Change.getRevision(): ContentRevision? {
+        return afterRevision ?: beforeRevision
     }
 }
